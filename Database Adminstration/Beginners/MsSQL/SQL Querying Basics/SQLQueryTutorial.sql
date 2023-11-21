64856b57a54b1aeb9d17d20559b6d5f3536eefd1
piusmwilson <piusmnwilson@gmail.com>
2023-11-21 12:18:43 +0300

    --

--- Database Adminstration/Beginners/MsSQL/SQL Querying Basics/SQLQueryTutorial.sql
+++ Database Adminstration/Beginners/MsSQL/SQL Querying Basics/SQLQueryTutorial.sql
@@ -42,3 +42,86 @@ EmployeeID,LastName,FirstName,BirthDate,Country,HomePhone,
   FROM Employees WHERE Country ='USA'
 -----------------------------------------------------------------
 
+--Extend filter with another additional clause to get more detailed results --AND CLAUSE
+SELECT 
+EmployeeID,LastName,FirstName,BirthDate,Country,HomePhone,
+[Region],[City],[PostalCode]
+  FROM Employees WHERE Country ='USA' AND City ='Seattle'
+-----------------------------------------------------------------
+
+--Extend filter with another additional clause to get more detailed results --OR CLAUSE
+SELECT 
+EmployeeID,LastName,FirstName,BirthDate,Country,HomePhone,
+[Region],[City],[PostalCode]
+  FROM Employees WHERE Country ='USA' OR City ='Seattle'
+-----------------------------------------------------------------
+SELECT 
+EmployeeID,LastName,FirstName,BirthDate,Country,HomePhone,
+[Region],[City],[PostalCode]
+  FROM Employees WHERE [FirstName] ='Janet' OR [LastName] ='Davolio'
+-----------------------------------------------------------------
+
+--Exercise One
+SELECT 
+[LastName],[FirstName],[Address],[City],[Region],[Country],[PostalCode]
+ FROM Employees WHERE COUNTRY = 'USA' OR LastName = 'Buchanan'
+ ----------------------------------------------------------------------
+
+--NULL, NOT NULL
+ SELECT TOP (1000) [OrderID]
+      ,[CustomerID],[EmployeeID]
+      ,[OrderDate]
+      ,[RequiredDate]
+      ,[ShippedDate]
+      ,[ShipVia]
+      ,[Freight]
+      ,[ShipName]
+      ,[ShipAddress]
+      ,[ShipCity]
+      ,[ShipRegion]
+      ,[ShipPostalCode]
+      ,[ShipCountry]
+  FROM [master].[dbo].[Orders]
+  WHERE [ShipRegion] IS NULL
+--------
+ SELECT TOP (1000) [OrderID]
+      ,[CustomerID]
+      ,[EmployeeID]
+      ,[OrderDate]
+      ,[RequiredDate]
+      ,[ShippedDate]
+      ,[ShipVia]
+      ,[Freight]
+      ,[ShipName]
+      ,[ShipAddress]
+      ,[ShipCity]
+      ,[ShipRegion]
+      ,[ShipPostalCode]
+      ,[ShipCountry]
+  FROM [master].[dbo].[Orders]
+  WHERE [ShipRegion] IS NOT NULL
+
+--BETWEEN, IN
+SELECT TOP (1000) [ProductID]
+      ,[ProductName]
+      ,[SupplierID]
+      ,[CategoryID]
+      ,[QuantityPerUnit]
+      ,[UnitPrice]
+      ,[UnitsInStock]
+      ,[UnitsOnOrder]
+      ,[ReorderLevel]
+      ,[Discontinued]
+  FROM [master].[dbo].[Products] WHERE UnitPrice BETWEEN '18.00' AND '22.00'--BETWEEN
+
+SELECT TOP (1000) [ProductID]
+      ,[ProductName]
+      ,[SupplierID]
+      ,[CategoryID]
+      ,[QuantityPerUnit]
+      ,[UnitPrice]
+      ,[UnitsInStock]
+      ,[UnitsOnOrder]
+      ,[ReorderLevel]
+      ,[Discontinued]
+  FROM [master].[dbo].[Products] WHERE SupplierID IN (1,2,5)--IN
