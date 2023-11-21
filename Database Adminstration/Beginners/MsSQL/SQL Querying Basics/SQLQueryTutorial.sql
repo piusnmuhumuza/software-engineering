84c304a37c8f73bc61d694dcc4974dd58130e3e6
piusmwilson <piusmnwilson@gmail.com>
2023-11-21 15:00:27 +0300

    --

--- Database Adminstration/Beginners/MsSQL/SQL Querying Basics/SQLQueryTutorial.sql
+++ Database Adminstration/Beginners/MsSQL/SQL Querying Basics/SQLQueryTutorial.sql
@@ -209,3 +209,78 @@ SELECT TOP (1000) [OrderID]
       ,[ShipPostalCode]
       ,[ShipCountry]
   FROM [master].[dbo].[Orders]
+
+
+--AVG SUM COUNT MIN MAX
+-----------------------------------------
+
+--COUNT - How many products do i have?
+  Select count(ProductID)FROM [master].[dbo].[Products]
+  Select count(orderid)FROM [master].[dbo].[Order Details]
+--------------------------------------------------------
+--AVG - What is the Average price of all my products?
+  Select AVG(UnitPrice)FROM [master].[dbo].[Products]
+-------------------------------------------------------
+--SUM - What is the accumulated value of all my unit prices?
+  Select sum(UnitPrice)FROM [master].[dbo].[Order Details]
+-----------------------------------------------------------
+--MIN - What is the minimum Unit price in all my products?!
+ Select min(UnitPrice)FROM [master].[dbo].[Products]
+-------------------------------------------------------------
+--MAX - What is the maximum unit price in all my products?
+ Select max(UnitPrice)FROM [master].[dbo].[Products]
+------------------------------------------------------------
+
+-------------------------------------------------------
+--CONCAT(field, field)
+--1) Concatinate last Name and Title of the make formal last name
+--SUBSTRING (field, start, num of characters)
+--2) Show the First 5 Characters of the title field.
+SELECT TOP (1000)
+[EmployeeID],CONCAT(TitleOfCourtesy,FirstName ) AS FormalName --without space
+	  ,[LastName],[FirstName]
+	  ,CONCAT(FirstName,' ', LastName) AS FullName
+      ,[Title]
+	  ,SUBSTRING (Title, 1, 5) AS PartialTitles
+,[TitleOfCourtesy],[BirthDate],[HireDate],[Address],[City],[Region]
+,[PostalCode],[Country],[HomePhone],[Extension],[Photo],[Notes],[ReportsTo],[PhotoPath]
+  FROM [master].[dbo].[Employees]
+
+
+--ORDER BY
+SELECT 
+[OrderID],[ProductID],[UnitPrice],[Quantity],[Discount]
+  FROM [master].[dbo].[Order Details] ORDER BY UnitPrice ASC --ASCENDING.
+----------------------------------------------------------------------
+SELECT 
+[OrderID],[ProductID],[UnitPrice],[Quantity],[Discount]
+  FROM [master].[dbo].[Order Details] ORDER BY UnitPrice DESC --DESCENDING.
+----------------------------------------------------------------------
+--GROUP BY
+SELECT 
+[OrderID]
+--,[ProductID]
+,SUM (UnitPrice) AS TotalPrice
+--,[Quantity],[Discount]
+  FROM [master].[dbo].[Order Details] GROUP BY OrderID
+
+--RANKING
+SELECT 
+[SupplierID],[CompanyName],[Country]
+,RANK () OVER (
+ORDER BY Country ASC
+) SC_RANK
+  FROM [master].[dbo].[Suppliers] ORDER BY Country
+
+
+--HAVING
+SELECT 
+     COUNT([OrderID])NumOrders
+      ,[ProductID]
+      ,SUM(UnitPrice) TotalUnitPrice
+  FROM [master].[dbo].[Order Details] 
+  GROUP BY ProductID
+  HAVING SUM(UnitPrice) > 400
+
+
+
