03cd1b9b330c5fd98333cc1a3fbb11be2a77a734
piusmwilson <piusmnwilson@gmail.com>
2023-11-22 11:44:46 +0300

    --

--- Database Adminstration/Beginners/MySQL- SQL For Beginners/MySQLTutorial.sql
+++ Database Adminstration/Beginners/MySQL- SQL For Beginners/MySQLTutorial.sql
@@ -108,3 +108,11 @@ WHERE birth_date > '1990-01-01' OR (points > 1000 AND state = 'VA');
 SELECT * 
 FROM Customers
 WHERE NOT (birth_date > '1990-01-01' OR points > 1000);
+
+--
+-- From order_items table, get the items
+-- for order #6
+-- where the total price is greater than 30
+SELECT * 
+FROM order_items
+WHERE order_id = 6 AND unit_price * quantity > 30;
