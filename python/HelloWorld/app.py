06127fa1d6a8fa1262c12685a3e163a57d1a5262
piusmwilson <piusmnwilson@gmail.com>
2024-01-01 13:49:35 +0300

    --variables

--- python/HelloWorld/app.py
+++ python/HelloWorld/app.py
@@ -1,4 +1,29 @@
-print('Pius Muhumuza')
-print('O----')
-print(' ||||')
-print('*' * 10)
+# INTRODUCTION
+# ------------------------
+# print('Pius Muhumuza')
+# print('O----')
+#print(' ||||')
+# print('*' * 10)
+
+# VARIABLES
+# --------------
+## price = 10
+## price = 20 
+## rating = 4.9 ## floating point
+## name = 'Pius' ##string 
+## is_published = False ## boolean
+##print(price)
+
+
+# --------------------
+## Exercise
+### We check in a patient named John Smith. He's 20 years old
+### is a new patient.
+
+name = 'John Smith'
+age = 20
+is_new = True
+print(name)
+print(is_new)
+
+
