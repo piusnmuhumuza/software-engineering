e7fb5d57806121399a1089ba075bfed13035cc8a
piusmwilson <piusmnwilson@gmail.com>
2024-01-01 14:04:12 +0300

    --getting input

--- python/HelloWorld/app.py
+++ python/HelloWorld/app.py
@@ -20,10 +20,15 @@
 ### We check in a patient named John Smith. He's 20 years old
 ### is a new patient.
 
-name = 'John Smith'
-age = 20
-is_new = True
-print(name)
-print(is_new)
+## name = 'John Smith'
+## age = 20
+## is_new = True
+## print(name)
+## print(is_new)
 
-
+#-------------------------------
+# Getting Input
+name = input('What is your name? ')
+favourite_color = input('What is your favourite color? ')
+print('Hi ' + name)
+print(name +  ' likes '  + favourite_color)
