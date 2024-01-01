7948a01adcab232c097802d935090181a1becad8
piusmwilson <piusmnwilson@gmail.com>
2024-01-01 16:14:27 +0300

    -Type Conversion

--- python/HelloWorld/app.py
+++ python/HelloWorld/app.py
@@ -33,4 +33,16 @@
 ## print('Hi ' + name)
 ## print(name +  ' likes '  + favourite_color)
 
+#-------------------------------------
+# Type Conversion
+## birth_year = input('Birth year: ')
+## print(type(birth_year))
+## age = 2024 - int(birth_year)
+## print(type(age))
+## print(age)
+
+### Exercise
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
 
