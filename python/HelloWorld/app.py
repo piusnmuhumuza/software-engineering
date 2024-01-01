cc1e01631824e73a6004a6a6dec21ed12e3e30f0
piusmwilson <piusmnwilson@gmail.com>
2024-01-01 14:24:45 +0300

    --Getting Input

--- python/HelloWorld/app.py
+++ python/HelloWorld/app.py
@@ -28,7 +28,9 @@
 
 #-------------------------------
 # Getting Input
-name = input('What is your name? ')
-favourite_color = input('What is your favourite color? ')
-print('Hi ' + name)
-print(name +  ' likes '  + favourite_color)
+## name = input('What is your name? ')
+## favourite_color = input('What is your favourite color? ')
+## print('Hi ' + name)
+## print(name +  ' likes '  + favourite_color)
+
+
