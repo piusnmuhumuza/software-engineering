a75822b87cdc4c8580d1eca109e971fb5dc83c73
piusmwilson <piusmnwilson@gmail.com>
2024-01-03 08:32:09 +0300

    --

--- python/app.py
+++ python/app.py
@@ -71,3 +71,11 @@
 #------------------------------
 # String Methods
 
+## weight = int(input('weight: '))
+## unit = input('(L)bs or (K)g: ')
+## if unit.upper() == "L":
+#    converted = weight * 0.45
+#    print(f"You are {converted} Kilos")
+## else:
+#    converted = weight / 0.45
+#    print(f"You are {converted} pounds")
